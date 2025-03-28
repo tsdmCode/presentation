55afb0128d5dc116f05b6b172a4b31bf8c720562
tsdmCode <116952397+tsdmCode@users.noreply.github.com>
2025-03-28 13:21:39 +0100

    Add exclamation mark for emphasis and updated title

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,11 +2,11 @@ const pres = Array.from(document.getElementsByClassName('text'));
 let n = 0;
 
 const textArr = [
-  'HEJ BROBYGGERE',
+  'HEJ BROBYGGERE!',
   'Her på webdev bruger vi mange teknologier, så som HTML, CSS og Javascript.',
   'HTML er "byggeblokkene" til hjemmesiden, det er det der har defineret at der skal være de har tekststumper ol.',
   'CSS er det visuelle, det er det som bestemmer hvor stor teksten skal være, hvilken farve osv. osv.',
-  'JavaScript tager sig af funktionaliteten, og kan beskrives som husets elektricitet. Det er også javascript, som får det her tekst til at dukke op.',
+  'JavaScript tager sig af funktionaliteten, og kan beskrives som husets elektricitet. Det er også JavaScript, som får denne her tekst til at dukke op som den gør.',
 ];
 function print() {
   if (n >= textArr.length || n >= pres.length) return;
@@ -16,8 +16,9 @@ function print() {
 
   for (let i = 0; i < word.length; i++) {
     setTimeout(() => {
+      console.log(word[i]);
       el.innerHTML += word[i];
-    }, i * 25);
+    }, i * 75);
   }
   n++;
 }
